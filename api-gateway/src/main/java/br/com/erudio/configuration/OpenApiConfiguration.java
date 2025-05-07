4e3951054c33cdf599fbdb412adb4a5a54fa7baa
oLucasNascimento <lucasf.ferreira@bandtec.com.br>
2025-05-07 08:55:19 -0300

    Adding and Configuring API-Gateway to project

---
+++ api-gateway/src/main/java/br/com/erudio/configuration/OpenApiConfiguration.java
@@ -0,0 +1,47 @@
+package br.com.erudio.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import jakarta.ws.rs.HttpMethod;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springdoc.core.properties.SwaggerUiConfigParameters;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@OpenAPIDefinition
+@Configuration
+public class OpenApiConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info()
+                .title("API Gateway Service")
+                .description("API Gateway Service")
+                .version("1.0.0"));
+    }
+
+//    @Bean
+//    @Lazy(false)
+//    public List<GroupedOpenApi> apis(SwaggerUiConfigParameters config, RouteDefinitionLocator locator) {
+//        var definitions = locator.getRouteDefinitions().collectList().block();
+//        definitions.stream()
+//                .filter(routeDefinition -> routeDefinition.getId().matches(".*-service"))
+//                .forEach(routeDefinition -> {
+//                    String name = routeDefinition.getId();
+//                    config.addGroup(name);
+//                    GroupedOpenApi.builder().pathsToMatch("/" + name + "/**")
+//                            .group(name).build();
+//                });
+//        return new ArrayList<>();
+//    }
+
+
+}
